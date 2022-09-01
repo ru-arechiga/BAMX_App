b19ce3b4c84e7203b1a34b460ec264885aaa61bd
ru_arechiga <a01634610@tec.mx>
2022-09-01 00:21:28 -0500

    Intento de fragmento sobre BAMX

--- app/src/main/java/com/example/bamx_app/MainActivity.kt
+++ app/src/main/java/com/example/bamx_app/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fragmentoSobreBamx: SobreBamx
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentoSobreBamx = SobreBamx()
     }
 }
