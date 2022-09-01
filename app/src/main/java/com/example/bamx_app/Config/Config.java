6102de2d523b571c61568485afe47ea12b60bb3c
Hector <hecrafalac@hotmail.com>
2022-09-01 00:36:34 -0500

    Avance de donaciones a traves de paypal

---
+++ app/src/main/java/com/example/bamx_app/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.bamx_app.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "Ab8Vtkqbj_P18zpvCSicVPtHID_DSA_YxBadeHUHzQn2XbxUyc2xuW_Yc2NqQpHd_u4uhYSzTDdNpCtW";
+}
