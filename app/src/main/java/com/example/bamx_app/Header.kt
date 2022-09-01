2444d30450982ed4d707b36df8c994f1cc98b6ce
ru-arechiga <A01634610@tec.mx>
2022-09-01 08:36:56 -0500

    Modificaciones a header y footer

---
+++ app/src/main/java/com/example/bamx_app/Header.kt
@@ -0,0 +1,11 @@
+package com.example.bamx_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Header : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_header)
+    }
+}
