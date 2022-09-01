e38f749bfe67b6c8de719cab0df7e7004c167e7a
a01639224 <a01639224@itesm.mx>
2022-09-01 09:07:36 -0500

    Redes sociales funcionales

--- app/src/main/java/com/example/bamx_app/Header.kt
+++ app/src/main/java/com/example/bamx_app/Header.kt
@@ -1,9 +1,17 @@
 package com.example.bamx_app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 class Header : AppCompatActivity() {
+
+    lateinit var facebookLogo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_header)
--- app/src/main/java/com/example/bamx_app/MainActivity.kt
+++ app/src/main/java/com/example/bamx_app/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.bamx_app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         fragmentoSobreBamx = SobreBamx()
+    }
+
+    fun facebook(view: View?) {
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("https://www.facebook.com/BDAGDL/")
+        startActivity(openURL)
+    }
+
+    fun twitter(view: View?) {
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("https://twitter.com/BDAGDL")
+        startActivity(openURL)
+    }
+
+    fun instagram(view: View?) {
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("https://www.instagram.com/bda_guadalajara/")
+        startActivity(openURL)
     }
 }
