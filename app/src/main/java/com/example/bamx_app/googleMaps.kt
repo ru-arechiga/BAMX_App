1ba0dc75570baed997b7ebcf167950edb4587989
Hect0r1 <hecrafalac@hotmail.com>
2022-09-09 15:44:09 -0500

    Agregue el mapa de la ubicación de BAMX y agregue el texto de la pagina sobre bamx

---
+++ app/src/main/java/com/example/bamx_app/googleMaps.kt
@@ -0,0 +1,38 @@
+package com.example.bamx_app
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class googleMaps : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val bamxGDL = LatLng(20.65648952263486, -103.35539031827524)
+        googleMap.addMarker(MarkerOptions().position(bamxGDL).title("BAMX GDL"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bamxGDL, 18f))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_google_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
